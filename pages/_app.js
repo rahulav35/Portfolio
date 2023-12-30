ae0c6bc9731ae87f049f8cc1c1629192d032be4c
rahulav35 <rahulmohanav35@gmail.com>
2023-12-30 22:57:25 +0530

    MinorChange

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import '../styles/globals.css';
 import Layout from "../components/Layout";
+import Transition from "../components/Transition";
+
+
+// next router
+import { useRouter } from "next/router";
+
+// framer motion
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
 
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>;
+  const router = useRouter();
+
+
+  return (<Layout>
+
+    <AnimatePresence mode='wait'>
+      <motion.div key={router.route} className='h-full'>
+        <Transition/>
+        <Component {...pageProps} />
+
+      </motion.div>
+    </AnimatePresence>
+
+   
+
+  </Layout>) ;
 }
 
 export default MyApp;
