c3d41d7e5414020b9168adcd49cec10a14a2370e
rahulav35 <rahulmohanav35@gmail.com>
2024-01-01 19:27:01 +0530

    edited

--- components/Transition.js
+++ components/Transition.js
@@ -21,17 +21,43 @@ const Transition = () => {
   return (
     <>
       <motion.div
-        className=" fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]    "
+        className= "fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
         exit="exit"
-        Transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+        transition={{delay:0.2 , duration:0.6 , ease: 'easeInOut'}}
       >
         
       </motion.div>
-      <div className="">2</div>
-      <div className="">3</div>
+      
+      
+      <motion.div
+        className= "fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{delay:0.4 , duration:0.6 , ease: 'easeInOut'}}
+      >
+        
+      </motion.div>
+
+
+
+      <motion.div
+        className= "fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{delay:0.6 , duration:0.6 , ease: 'easeInOut'}}
+      >
+        
+      </motion.div>
+
+
+
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+// next image
+import image from "next/image";
+
+
+// components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+// framer motion
+import {motion} from "framer-motion";
+
+// variants
+import { fadeIn } from "../variants";
+
+
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="bg-primary/60 h-full">
+
+    <div className="hw-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+
+      <div className=""> 
+
+      </div>
+
+    </div>
+
+  </div>;
 };
 
 export default Home;
