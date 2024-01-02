d0d605067686b33e59185368c64fccdebe81c40b
rahulav35 <rahulmohanav35@gmail.com>
2024-01-02 21:34:17 +0530

    btnAdded

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,48 @@ const Home = () => {
 
     <div className="hw-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
 
-      <div className=""> 
+      <div className="text-center flex flex-col justify-center xl:pt-40 box-border xl:text-left h-full container mx-auto"> 
+
+{/* title */}
+<motion.h1
+variants={fadeIn('down', 0.2)}
+initial='hidden'
+animate='show'
+exit='hidden'
+
+ className="h1"> Transforming Ideas <br/> Info {' '}
+<span className="text-accent"> Digital Reality</span>
+
+</motion.h1>
+
+{/* subtitle */}
+<motion.p 
+variants={fadeIn('down', 0.3)}
+initial='hidden'
+animate='show'
+exit='hidden'
+
+className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+  hgf ksmdlksn nihudhksnckn njhn nlfmdkfn ufuhfkfn nihfijf nhujb nfhifkfnkdf  knfkdnfdff.idhfidhfnnfdf.fihfif jbfuf jhff bgygygw  jhshfnjfgb jushfuhff jfe7fguwdhwknd knkjdwihdufbj jbfgdbdbsj 
+</motion.p>
+
+{/* btn */}
+
+<div className="flex justify-center xl:hidden relative">
+  <ProjectsBtn/>
+</div>
+
+<motion.div 
+variants={fadeIn('down', 0.4)}
+initial='hidden'
+animate='show'
+exit='hidden'
+className="hidden xl:flex"
+>
+  <ProjectsBtn/>
+</motion.div>
+
+
 
       </div>
 
