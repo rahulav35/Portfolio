a43802f1dad44a56835888527c62f2f229374073
rahulav35 <rahulmohanav35@gmail.com>
2024-01-14 19:57:56 +0530

    aboutStarted

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable react/jsx-no-undef */
+import React, {useState} from "react";
+
+
 // icons
 import {
   FaHtml5,
@@ -88,8 +92,67 @@ const aboutData = [
   },
 ];
 
+
+// components
+
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+
+// framer motion
+
+import { motion } from "framer-motion";
+import {fadeIn} from "../../variants"
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left box-border ">
+    <Circles/>
+    {/* avatar img */}
+
+
+    <motion.div 
+    variants={fadeIn('right',0.2)}
+    initial='hidden'
+    animate='show'
+    exit='hidden'
+
+    className="hidden xl:flex absolute bottom-0 -left-[370px]">
+      <Avatar/>
+
+    </motion.div>
+    <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+      <div>text</div>
+      <div>
+        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+          {aboutData.map((item,itemIndex)=>{
+            return(
+              <div key={itemIndex}
+              className={`${index === itemIndex && 'text-accent after:w-[100%] after:transition-all after:duration-300'}
+              cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0
+              `}
+              onClick={()=> setIndex(itemIndex)}
+              
+              >
+                {item.title}
+
+              </div>
+            )
+          })}
+
+        </div>
+
+      </div>
+    </div>
+
+
+
+   
+
+  </div>
+  );
 };
 
 export default About;
